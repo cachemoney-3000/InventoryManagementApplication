06a122c0631d1e193d63fc1848a30a5db5a9ec1f
joshuas029 <jsamontanez@knights.ucf.edu>
2021-11-14 13:40:17 -0500

    added comments and updated psuedocodes

--- src/main/java/baseline/Controller.java
+++ src/main/java/baseline/Controller.java
@@ -14,10 +14,12 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
@@ -62,32 +64,62 @@ public class Controller implements Initializable {
 
     private ObservableList<InventoryModel> itemList = FXCollections.observableArrayList();
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        /*
+        Shall #8, #9, #10 is already handled by the table view
+        The table view can sort the data based on the item name, item value, and serial number
+         */
+
+        // These will not allow user to resize the columns
+        itemValue.setResizable(false);
+        itemSerialNumber.setResizable(false);
+        itemName.setResizable(false);
+
+        cellPropertyValue();
+        editTableContents();
+        textFieldListener();
+        searchItem();
+        removeItem();
+        warningHandler();
+    }
+
     @FXML
     void addItem(MouseEvent event) {
+        // Satisfies: shall #1, #1.1, #1.2, #1.3, #2
         if(event.getSource() == addButton){
             UpdateInventory inventory = new UpdateInventory();
 
+            // These will convert all the inputs from the all the text fields into a string
             String item = itemNameTextField.getText();
-            String itemValue = itemValueTextField.getText();
+            String itemValue = "$" + itemValueTextField.getText();
             String itemSerial = itemSerialNumberTextField.getText();
 
+            // If there are no duplicate names or serial number, it will remove the inputs from all the text field
+            // But if a duplicate item was found, it will not remove the inputs, so it can be easily edited by the user
             if(!itemList.contains(new InventoryModel(item, itemValue, itemSerial))){
                 itemNameTextField.setText("");
                 itemValueTextField.setText("");
                 itemSerialNumberTextField.setText("");
             }
 
+            // This will call the add item method and will handle adding new items into the list
+            // Assuming there are no warnings and the inputs passed the requirements
             itemList = inventory.addItem(item, itemValue, itemSerial, itemList);
-            table.setItems(itemList);
 
+            // The newly added items will be added into the table
+            table.setItems(itemList);
         }
     }
 
     @FXML
     void clearItems(MouseEvent event) {
+        // Satisfies shall #4
         if(event.getSource() == clearButton){
             UpdateInventory inventory = new UpdateInventory();
+            // This will clear all the items inside the table
             table.getItems().clear();
+            // This will clear all the items inside the observable list
             itemList = inventory.clearInventory(itemList);
         }
     }
@@ -164,46 +196,45 @@ public class Controller implements Initializable {
         }
     }
 
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        itemValue.setResizable(false);
-        itemSerialNumber.setResizable(false);
-        itemName.setResizable(false);
-
-        cellPropertyValue();
-        editTableContents();
-        textFieldListener();
-        searchItem();
-        removeItem();
-        warningHandler();
-
-    }
-
+    // This method will determine which data will be put into a column
     private void cellPropertyValue(){
+        // The item name should be in the item name column
         itemName.setCellValueFactory(
                 new PropertyValueFactory<>("itemName")
         );
+        // The item value should be in the value column
         itemValue.setCellValueFactory(
                 new PropertyValueFactory<>("value")
         );
+        // The item serial number should be in the serial number column
         itemSerialNumber.setCellValueFactory(
                 new PropertyValueFactory<>("itemSerialNumber")
         );
     }
 
     private void warningHandler(){
+        // This method is for showing all the warnings if the user input doesn't satisfy a requirement
         String invalid = "Invalid Value!";
 
-        nameTextFieldWarnings(invalid);
-        valueTextFieldWarnings(invalid);
+        editItemNameWarnings(invalid);
+        editValueWarnings(invalid);
         serialTextFieldWarnings(invalid);
     }
 
-    private void nameTextFieldWarnings(String invalid){
+    // This method satisfies: shall #7.1
+    private void editItemNameWarnings(String invalid){
+        /*
+        The program will show warnings if the item name is:
+        - Less than 2 or more than 256 characters
+        - The item already exist
+         */
+
+        // This is a listener for any change made from the item name column
         itemName.setOnEditCommit(event -> {
+            // It will store the new value into the 'newValue' string
             String newValue = event.getNewValue();
 
+            // It will then check if it passes all the requirements listed above
             if(Objects.equals(newValue, "") ||
                     newValue.length() < 2 || newValue.length() > 256){
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -221,19 +252,31 @@ public class Controller implements Initializable {
                 alert.showAndWait();
             }
 
+            // If it passed all the requirements, the new value will appear in the table
             else{
                 event.getRowValue().setItemName(newValue);
             }
 
+            // This is for fixing a bug where, the new value doesn't appear correctly
             itemName.setVisible(false);
             itemName.setVisible(true);
         });
     }
 
-    private void valueTextFieldWarnings(String invalid){
+    // This method satisfies: shall #5.1
+    private void editValueWarnings(String invalid){
+        /*
+        This method will show warning if:
+        - A new value is empty
+        - The value is not in digits greater or equal than 0
+         */
+
+        // This is a listener for any change made from the item value column
         itemValue.setOnEditCommit(event -> {
+            // The new value will be stored in this string
             String newValue = event.getNewValue();
 
+            // It will then check if it passes the requirements listed above. If not, it will show a warning
             if(Objects.equals(newValue, "")){
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle(invalid);
@@ -243,6 +286,7 @@ public class Controller implements Initializable {
 
             }
 
+            // If the input passed all the requirements, it will now be added into the table
             if(newValue.matches("[0-9]+")){
                 System.out.println("Input valid");
                 event.getRowValue().setValue(newValue);
@@ -256,15 +300,27 @@ public class Controller implements Initializable {
                 alert.showAndWait();
             }
 
+            // This is for fixing a bug where, the new value doesn't appear correctly
             itemValue.setVisible(false);
             itemValue.setVisible(true);
         });
     }
 
+    // This method satisfies shall #6.1
     private void serialTextFieldWarnings(String invalid){
+        /*
+        This method will show warning if:
+        - A new serial number is empty
+        - The serial number already exist
+        - The serial number doesn't follow the format (A-XXX-XXX-XXX)
+         */
+
+        // This is a listener for any change made from the item serial number column
         itemSerialNumber.setOnEditCommit(event -> {
+            // It will store the value into this string
             String newValue = event.getNewValue();
 
+            // It will then check if the new value passes all the requirements. If not, it will show an error
             if (Objects.equals(newValue, "")) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle(invalid);
@@ -281,6 +337,7 @@ public class Controller implements Initializable {
                 alert.showAndWait();
             }
 
+            // If the input passes all the requirements, it will update the table
             else{
                 event.getRowValue().setItemName(newValue);
             }
@@ -297,21 +354,31 @@ public class Controller implements Initializable {
                 alert.showAndWait();
             }
 
+            // This is for fixing a bug where, the new value doesn't appear correctly
             itemSerialNumber.setVisible(false);
             itemSerialNumber.setVisible(true);
         });
     }
 
-
+    // Satisfies shall #3
     private void removeItem(){
+        // This will automatically remove an item from the table and on the observable list
         removeButton.setOnAction(e -> {
             InventoryModel selectedItem = table.getSelectionModel().getSelectedItem();
-            System.out.println(selectedItem);
             table.getItems().remove(selectedItem);
         });
     }
 
+    // This method satisfies shall #5, #6, #7
     private void editTableContents() {
+        /*
+        This allows user to edit the value of an existing inventory item.
+        It can edit the:
+        - Item Name
+        - Item Value
+        - Item Serial Number
+         */
+
         itemName.setCellFactory(TextFieldTableCell.forTableColumn());
 
         itemName.setOnEditCommit(e ->
@@ -327,7 +394,7 @@ public class Controller implements Initializable {
         itemSerialNumber.setOnEditCommit(e ->
                 e.getTableView().getItems().get(e.getTablePosition().getRow()).setItemSerialNumber(e.getNewValue()));
 
-
+        // Make the table data editable
         table.setEditable(true);
     }
 
@@ -376,7 +443,7 @@ public class Controller implements Initializable {
             if(itemSerialNumberTextField.getText().matches("[A-Za-z]+-[A-Za-z0-9]{1,3}-[A-Za-z0-9]{1,3}-[A-Za-z0-9]{1,3}")){
                 System.out.println("Serial Valid.");
             }
-            
+
             else{
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle(invalid);
@@ -422,6 +489,4 @@ public class Controller implements Initializable {
         return (inventory.getItemName().toLowerCase().contains(searchText)) ||
                 (inventory.getItemSerialNumber().toLowerCase().contains(searchText));
     }
-
-
 }
--- src/main/java/baseline/UpdateInventory.java
+++ src/main/java/baseline/UpdateInventory.java
@@ -2,12 +2,13 @@ package baseline;
 
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TableView;
 
 public class UpdateInventory {
     public ObservableList<InventoryModel> addItem(String item, String itemValue, String itemSerial,
                                                   ObservableList<InventoryModel> itemList){
 
+        // Shall #2.1
+        // If an item name or serial number already exists, it will show an error
         if(itemList.contains(new InventoryModel(item, itemValue, itemSerial))){
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Item already exist!");
@@ -16,6 +17,7 @@ public class UpdateInventory {
             alert.showAndWait();
         }
 
+        // If the inputs passed all the requirements, it will be added into the list
         else
             itemList.add(new InventoryModel(item, itemValue, itemSerial));
 
@@ -23,7 +25,9 @@ public class UpdateInventory {
     }
 
     public ObservableList<InventoryModel> clearInventory(ObservableList<InventoryModel> itemList){
+        // This will clear all the items inside the observable list
         itemList.clear();
+
         return itemList;
     }
 
